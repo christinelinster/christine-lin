dd267c5a5886cb3d52426ba217c0048b0462e5ac
Christine Lin <lin.christine19@gmail.com>
2025-02-11 19:26:39 -0800

    add typewriter effect to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,9 @@
-import { Github, Linkedin, Mail } from 'lucide-react';
-import { Link } from 'react-router-dom';
-import '../styles/home.css';
+import { Github, Linkedin, Mail } from "lucide-react";
+import { Link } from "react-router-dom";
+import Typewriter from "typewriter-effect";
+import "../styles/home.css";
 
-//Add typewriter effect to my name. 
+//Add typewriter effect to my name.
 
 export default function Home() {
   return (
@@ -13,24 +14,38 @@ export default function Home() {
             <span className="block">Hello, I'm</span>
             <span className="home-name">Christine Lin</span>
           </h2>
-    
+          
+
           <p className="home-description">
-          Software Developer. Practice Lead. Lifelong Student.
+          <Typewriter
+          options={{
+            autoStart: true,
+            loop: true,
+          }}
+            onInit={(typewriter) => {
+              typewriter
+              .changeDelay(50)
+                .typeString("Software Developer | ")
+                .changeDelay(50)
+                .pauseFor(300)
+                .typeString("Practice Lead | ")
+                .changeDelay(50)
+                .pauseFor(300)
+                .typeString(" Lifelong Student")
+                .pauseFor(3000)
+                .deleteAll()
+                .start()
+            }}
+          />
           </p>
           <div className="button-container">
             <div>
-              <Link
-                to="/projects"
-                className="primary-button"
-              >
+              <Link to="/projects" className="primary-button">
                 View My Work
               </Link>
             </div>
             <div>
-              <Link
-                to="/contact"
-                className="secondary-button"
-              >
+              <Link to="/contact" className="secondary-button">
                 Contact Me
               </Link>
             </div>
@@ -52,10 +67,7 @@ export default function Home() {
             >
               <Linkedin className="h-6 w-6" />
             </a>
-            <a
-              href="mailto:lin.christine19@gmail.com"
-              className="social-link"
-            >
+            <a href="mailto:lin.christine19@gmail.com" className="social-link">
               <Mail className="h-6 w-6" />
             </a>
           </div>
